d48b3776fd1eb83880a7f6a609a38a17db5dbab4
savindu8 <119857048+savindu8@users.noreply.github.com>
2025-02-28 22:18:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=IT9jxZCS#BdkAXAKZE89sov2J5oYYFTtOXU9Czpa1BRR_gxnSYwI' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94764766973' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
